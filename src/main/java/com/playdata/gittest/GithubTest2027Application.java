94df6a2e20f5a03404f77d4e3b1cf4a74f9ca720
이호진 <gommind@naver.com>
2023-10-23 13:49:07 +0900

    git test

---
+++ src/main/java/com/playdata/gittest/GithubTest2027Application.java
@@ -0,0 +1,13 @@
+package com.playdata.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest2027Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest2027Application.class, args);
+	}
+
+}
